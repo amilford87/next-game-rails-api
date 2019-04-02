81ff48f971952de327b22b39fad0853df9b0e602
Matt Cook <mcc1985@gmail.com>
2019-04-02 20:29:40 +0000

    Find Next Games Algorithm
    Find existing games for user based on their sport preferences.

--- app/controllers/api/next_games_controller.rb
+++ app/controllers/api/next_games_controller.rb
@@ -9,35 +9,61 @@ class Api::NextGamesController < ApplicationController
     sports = user.sports
 
     # Find existing games
-    current_games = {}
+    existing_games = []
     sports.each do |sport|
-      current_games[sport.name] = []
       games = sport.games
-
+      games.each do |g|
+        existing_game = {
+          type: 'active',
+          date: g.date,
+          image: g.sport.image,
+          location: {
+            lat: g.facility.latitude,
+            lng: g.facility.longitude,
+            dist: (Geocoder::Calculations.distance_between([current_location[:location][:lat], current_location[:location][:lng]], [g.facility.latitude.to_f, g.facility.longitude.to_f]) * 1.60934).round(2)
+          },
+          facility: g.facility.name,
+          other_players: g.users.ids,
+          sport: g.sport.name,
+          time: g.start_time,
+        }
+        existing_games.push(existing_game)
+      end
+    end
 
     # Find empty games
-    new_parks = {}
+    empty_games = {}
     sports.each do |sport|
-      new_parks[sport.name] = []
+      empty_games[sport.name] = []
       facilities = sport.facilities
       facilities.each do |f|
-        facility = {
-          name: f.name,
+        new_game = {
+          type: 'new',
+          date: 'USER PREF HERE',
+          image: sport.image,
           location: {
             lat: f.latitude,
             lng: f.longitude,
             dist: (Geocoder::Calculations.distance_between([current_location[:location][:lat], current_location[:location][:lng]], [f.latitude.to_f, f.longitude.to_f]) * 1.60934).round(2)
-          }
+          },
+          facility: f.name,
+          other_players: 0,
+          sport: sport.name,
+          time: "USER PREF HERE"
         }
-        new_parks[sport.name].push(facility)
+        empty_games[sport.name].push(new_game)
       end
-      new_parks[sport.name].sort! { |x,y| x[:location][:dist] <=> y[:location][:dist]}
-      if new_parks[sport.name].count > 2
-        new_parks[sport.name] = new_parks[sport.name].slice(0,2)
+      empty_games[sport.name].sort! { |x,y| x[:location][:dist] <=> y[:location][:dist]}
+      if empty_games[sport.name].count > 2
+        empty_games[sport.name] = empty_games[sport.name].slice(0,2)
       end
     end
-    # Make sure the user gets a game!
-    empty_games = get_empty_games(user, current_location)
+    new_games = []
+    empty_games.each_value do |s|
+      s.each do |eg|
+        new_games.push(eg)
+      end
+    end
   end
 
   private
