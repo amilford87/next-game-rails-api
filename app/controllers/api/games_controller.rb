52a2de3fa358e5af8658e9fbf4218865d1276b08
amilford87 <amilford87@gmail.com>
2019-04-02 11:52:21 -0400

    formatted output json to display better info

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -4,7 +4,27 @@ class Api::GamesController < ApplicationController
   def index
     if @user = User.first
       @current_games = @user.games
-      render json: @current_games, status: 200
+      @user_current_games_data = []
+      @user.games.each do |game|
+        @game_sport = Sport.find(game.sport_id).name
+        @game_facility_name = Facility.find(game.facility_id).name
+        @game_facility_lat = Facility.find(game.facility_id).latitude
+        @game_facility_lng = Facility.find(game.facility_id).longitude
+        @game_other_players = []
+        game.user_ids.each do |user|
+          @my_games_usernames = User.find(user).username
+          @game_other_players.push(@my_games_usernames)
+        end
+        @users_saved_games = { other_players: @game_other_players.count,
+                        date: game.date.strftime("%A %d of %B %Y"),
+                        time: game.start_time.strftime("%I:%M %p"),
+                        sport: @game_sport,
+                        location: @game_facility_name,
+                        map: {lat: @game_facility_lat.to_f, lng: @game_facility_lng.to_f}
+                       }
+      @user_current_games_data.push(@users_saved_games) 
+      end
+      render json: @user_current_games_data, status: 200
     else
       render json: { message: 'incorrect credentials' }, status: 401
     end
