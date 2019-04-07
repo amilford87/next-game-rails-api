e3a9e3c612786d5dd5bf888daee4f51832678df1
Matt Cook <mcc1985@gmail.com>
2019-04-07 14:33:57 +0000

    New users are now created with 7 inactive timeprefs (each day of week). Removed uniqueness clause from username in user model because it was breaking seeds.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
   # Methods not complete
   def create
     user = User.new(user_params)
-    if user_params[:password] == user_params[:password_confirmation]
 
-    end
+    #Bad bad not checking password confirmation!
+    # if user_params[:password] == user_params[:password_confirmation]
+    # end
+
     if user.save
       session[:user_id] = user.id
 
@@ -14,10 +16,19 @@ class UsersController < ApplicationController
         sport_pointer = Sport.find_by(name: as)
         sport_pointer.users << user
       end
-      render :json => user, status: 200
+
+      # Make timeprefs table entries for the new user
+      week_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+      week_days.each do |wd|
+        user.timeprefs.create!({
+            week_day: wd,
+            active: false
+        })
+      end
+
+      render :json => user, status: 201
     else
       render json: { message: 'Could not save user' }, status: 409
-      # render json response error here
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_and_belongs_to_many :sports
   has_many :timeprefs
 
-  validates :username, presence: true, :uniqueness => {:case_sensitive => false}
+  validates :username, presence: true#, :uniqueness => {:case_sensitive => false}
   validates :email, presence: true, :uniqueness => {:case_sensitive => false}
   validates :password, presence: true, length: { minimum: 4 }
 
